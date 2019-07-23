98a8fe6357cb7f3602ef75223d936160e54f37be
Arron Lai <arron.laihongwei@gmail.com>
2019-07-23 21:44:06 +0800

    refine homepage

---
+++ docs/precache-manifest.2e6d995c4d6d3ce4c2844b7e504b30ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a89edd813dbfff3b33f14a53d3735cf8",
+    "url": "/github-card-app/index.html"
+  },
+  {
+    "revision": "07a47376ba8c36c9ab63",
+    "url": "/github-card-app/static/css/main.34aea5f6.chunk.css"
+  },
+  {
+    "revision": "80c81274019a053fad9f",
+    "url": "/github-card-app/static/js/2.59aed2a9.chunk.js"
+  },
+  {
+    "revision": "07a47376ba8c36c9ab63",
+    "url": "/github-card-app/static/js/main.896c77e5.chunk.js"
+  },
+  {
+    "revision": "9682929045e0c95b0003",
+    "url": "/github-card-app/static/js/runtime~main.e7acaa5f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-card-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.980481543861d87d71a9225c4df7ca12.js"
+  "/github-card-app/precache-manifest.2e6d995c4d6d3ce4c2844b7e504b30ce.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-card-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
